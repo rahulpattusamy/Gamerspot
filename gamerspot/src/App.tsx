754e437780b407bcbc2b6603eb16d707967df74c
rahul <rahulpattusamy@gmail.com>
2025-04-18 14:15:37 +0530

    build a responsive layout

---
+++ gamerspot/src/App.tsx
@@ -0,0 +1,16 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+
+const App = () => {
+  return (
+    <Grid templateAreas={{ base: `"nav main"`, lg: `"nav nav" "aside main"` }}>
+      <GridItem area="nav">coral</GridItem>
+      <Show above="lg">
+        <GridItem area="aside">Aside</GridItem>
+      </Show>
+
+      <GridItem area="main">Main</GridItem>
+    </Grid>
+  );
+};
+
+export default App;
