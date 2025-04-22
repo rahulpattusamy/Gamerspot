6d085245865b93866aa25800632059ea0b5bbddf
rahul <rahulpattusamy@gmail.com>
2025-04-22 08:59:55 +0530

    Displayed a Gamecard skeleton

--- gamerspot/src/components/GameGrid.tsx
+++ gamerspot/src/components/GameGrid.tsx
@@ -1,17 +1,28 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
+import GamecardContainer from "./GamecardContainer";
 
 const GameGrid = () => {
-  const { games, error } = useGames();
+  const { games, error, isLoading } = useGames();
+  const skeletons = [1, 2, 3, 4, 5, 6];
 
   return (
     <div>
-        {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{sm:1, md:2, lg:3, xl:4}
-      } spacing={10}>
+      {error && <Text>{error}</Text>}
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={10}>
+        {isLoading &&
+          skeletons.map((skeleton) => (
+            <GamecardContainer>
+              <GameCardSkeleton key={skeleton} />
+            </GamecardContainer>
+          ))}
+
         {games.map((game) => (
-          <GameCard game={game}/>
+          <GamecardContainer>
+            <GameCard game={game} />
+          </GamecardContainer>
         ))}
       </SimpleGrid>
     </div>
