3f13babc8d59a3a2db56c2733f6b350cb4778ce3
rahul <rahulpattusamy@gmail.com>
2025-04-24 11:01:36 +0530

    clean up the game card

--- gamerspot/src/components/GameCard.tsx
+++ gamerspot/src/components/GameCard.tsx
@@ -14,13 +14,14 @@ const GameCard = ({ game }: Props) => {
       <Card borderRadius={10} overflow={"hidden"}>
         <Img src={getCroppedImageUrl(game.background_image)} />
         <CardBody>
-          <Heading fontSize="2xl">{game.name}</Heading>
-          <HStack justifyContent="space-between">
+          
+          <HStack justifyContent="space-between" marginBottom={3}>
             <PlatformIconsList
               platforms={game.parent_platforms.map((p) => p.platform)}
             />
             <CriticScore score={game.metacritic} />
           </HStack>
+          <Heading fontSize="2xl">{game.name}</Heading>
         </CardBody>
       </Card>
     </div>
--- gamerspot/src/components/GameGrid.tsx
+++ gamerspot/src/components/GameGrid.tsx
@@ -16,7 +16,7 @@ const GameGrid = ({gamequery}:Props) => {
   return (
     <div>
       {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={3}>
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={6}>
         {isLoading &&
           skeletons.map((skeleton) => (
             <GamecardContainer key={skeleton}>
