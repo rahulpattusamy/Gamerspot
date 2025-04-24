47218f0ba07274479ff38b6e2c4542018d7f4796
rahul <rahulpattusamy@gmail.com>
2025-04-24 11:45:47 +0530

    #

--- gamerspot/src/components/GameGrid.tsx
+++ gamerspot/src/components/GameGrid.tsx
@@ -6,32 +6,30 @@ import GamecardContainer from "./GamecardContainer";
 import { GameQuery } from "../App";
 
 interface Props {
- gamequery: GameQuery;
+  gamequery: GameQuery;
 }
 
-const GameGrid = ({gamequery}:Props) => {
+const GameGrid = ({ gamequery }: Props) => {
   const { data, error, isLoading } = useGames(gamequery);
   const skeletons = [1, 2, 3, 4, 5, 6];
 
-  if(error)return<Text>{error}</Text>
+  if (error) return <Text>{error}</Text>;
 
   return (
-   
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={6}>
-        {isLoading &&
-          skeletons.map((skeleton) => (
-            <GamecardContainer key={skeleton}>
-              <GameCardSkeleton />
-            </GamecardContainer>
-          ))}
-
-        {data.map((game) => (
-          <GamecardContainer key={game.id}>
-            <GameCard game={game} />
+    <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={6}>
+      {isLoading &&
+        skeletons.map((skeleton) => (
+          <GamecardContainer key={skeleton}>
+            <GameCardSkeleton />
           </GamecardContainer>
         ))}
-      </SimpleGrid>
-    
+
+      {data.map((game) => (
+        <GamecardContainer key={game.id}>
+          <GameCard game={game} />
+        </GamecardContainer>
+      ))}
+    </SimpleGrid>
   );
 };
 
