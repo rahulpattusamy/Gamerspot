469ac46f791a1a7f97aba54571e82779c3624131
rahul <rahulpattusamy@gmail.com>
2025-06-21 15:29:22 +0530

    Build game attributes

--- gamerspot/src/entities/Game.ts
+++ gamerspot/src/entities/Game.ts
@@ -1,10 +1,13 @@
+import { Genre } from "./Genre";
 import { Platform } from "./Platform";
-
+import Publisher from "./Publisher";
 
 export interface Game {
   id: number;
   name: string;
   slug: string;
+  publishers: Publisher[];
+  genres: Genre[];
   background_image: string;
   parent_platforms: { platform: Platform }[];
   metacritic: number;
