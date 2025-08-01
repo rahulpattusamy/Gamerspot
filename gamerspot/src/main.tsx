370b2b5b73f3e074e6297cc3cbe55cdbb9a51d20
Rahulraj <146526803+rahulpattusamy@users.noreply.github.com>
2025-08-01 10:01:42 +0530

    Update main.tsx

--- gamerspot/src/main.tsx
+++ gamerspot/src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider,   } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools/production";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import theme from "./components/theme.ts";
 
@@ -17,7 +16,6 @@ createRoot(document.getElementById("root")!).render(
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <QueryClientProvider client={queryclient}>
         <RouterProvider router={router}/>
-        <ReactQueryDevtools/>
       </QueryClientProvider>
     </ChakraProvider>
   </StrictMode>
